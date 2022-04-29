b99e2600703439f535d8c81e0193c6a107edb149
LuoHaoAt2017 <hluo5206@gmail.com>
2022-04-29 21:27:46 +0800

    feat: svg

--- src/index/index.tsx
+++ src/index/index.tsx
@@ -5,6 +5,10 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <>Hello Index</>
+    <svg version="1.1" baseProfile="full" width={300} height={200} xmlns="http://www.w3.org/2000/svg">
+      <rect width="100%" height="100%" fill="red"></rect>
+      <circle cx={150} cy={100} r={80} fill="green"></circle>
+      <text x={150} y={125} fontSize={60} fill="white" textAnchor="middle">SVG</text>
+    </svg>
   </React.StrictMode>
 );
